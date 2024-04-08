f0da7248e8c1252c135479a2c39dc0fd50d21bb9
ClaudomiroSales <claudomiro.sales@gmail.com>
2024-04-08 10:19:08 -0300

    Update NomeAluno_Atividade3.h
    Finalização do arquivo de avaliação da Atividade 3.

--- Atividades/Atividade3/NomeAluno_Atividade3.h
+++ Atividades/Atividade3/NomeAluno_Atividade3.h
@@ -19,47 +19,74 @@ Todo set que recebe uma string deve limitar o tamanho da string usando o método
 Não colocar sets e gets no diagrama UML (mas precisam estar no código no repositório). 
 Sem o diagrama UML e vídeo o trabalho não será avaliado.*/
 
-//// Três constructores, sendo dois deles o constructor vazio e o constructor de cópia.
+//// Criar uma segunda classe. Todas as classes devem ter sempre dois contructores, 
+//sendo um de cópia, um atributo e um método que não  seja set ou get.
 
-     //No .h da classe
+      //O .h da classe 1 sem o set e gets
 
-     //Implementação no cpp da classe
-
-     //Uso no main dos três constructores
-
-//// Dois atributos não-const, sendo um deles uma string, e um ATRIBUTO const.
-
-     //No .h da classe
-
-     //Inicialização nos contructores
-
-     //Uso nos métodos
-
-//// Criar para cada atributo os métodos set e get
-
-     /*Todo set que recebe uma string deve limitar o tamanho da 
-     string usando o método substr.*/
-
-     //No .h da classe
-
-     //Implementação no cpp da classe
-
-     //Uso dos sets e gets no main
-
-
-//// A classe deve ter dois métodos, um que seja const e outro não const.
-
-     //Método const no .h da classe
-
-     //Implementação do método const no .cpp
-
-     //Uso do método const no main
+      //O .h da classe 2 sem o set e gets
      
-     //Método não-const no .h da classe
+      //Uso no main das duas classes
 
-     //Implementação do método não-const no .cpp
+//// Criar dois atributos static que não sejam arrays e nem const. 
+//Eles devem ser instanciados e usados efetivamente nas classes. 
 
-     //Uso do método não-const no main
+      //No .h da classe
 
-//// No main mostrar o uso da classe com objetos CONST e não-const da sua classe
+      //Inicialização 
+
+      //Uso efetivo nos métodos
+
+      //Uso desses métodos no main
+
+//// Criar um atributo const static e mostrar o uso dentro da classe.
+
+      //No .h da classe
+
+      //Inicialização 
+
+      //Uso efetivo nos métodos
+
+      //Uso desses métodos no main
+
+//// Criar dois métodos static e mostrar o uso no main. 
+
+      //No .h da classe
+
+      //Implementação
+    
+      //Uso desses métodos no main
+
+
+//// Ter pelo menos um método inline nas suas classes. 
+
+      //No .h da classe
+
+      //Implementação
+    
+      //Uso desses métodos no main
+
+//// Criar dois arrays dentro da classe, sendo um deles um const static. 
+//Mostrar o uso desses arrays na classe - não apenas inicialização e preenchimento. 
+
+      //No .h da classe
+
+      //Inicialização 
+
+      //Uso efetivo nos métodos - não apenas preenchimento
+
+      //Uso desses métodos no main
+
+
+//// Usar pelo menos dois loops, um for e um while, na sua classe.
+
+      //No cpp da classe
+    
+      //Uso no main dos métodos que tem os loops 
+
+
+/* Todo set que recebe uma string deve limitar o tamanho da string usando o método substr. Para fazer o diagrama usar o https://www.planttext.com/.
+Não colocar sets e gets no diagrama UML (mas precisam estar no código no repositório). 
+Sem o diagrama UML e vídeo o trabalho não será avaliado.
+
 
